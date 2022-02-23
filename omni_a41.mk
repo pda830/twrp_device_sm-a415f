4c5bbd632ecf2fd20a521e2580f32426d2905591
Riccardo Murachelli <64906631+riccardomurachelli@users.noreply.github.com>
2022-02-23 17:18:01 +0100

    First build
    dio ghane

---
+++ omni_a41.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from a41 device
+$(call inherit-product, device/samsung/a41/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a41
+PRODUCT_NAME := omni_a41
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A415F
+PRODUCT_MANUFACTURER := samsung
